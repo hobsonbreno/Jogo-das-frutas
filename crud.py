e7df4a8b5b76d24072dfe934d129553d182115dd
Hobson Nascimento <hobsonbmn@gmail.com>
2021-11-28 01:16:27 -0300

    atualização de formulario

--- crud.py
+++ crud.py
@@ -3,6 +3,6 @@ import streamlit as st;
 with st.form(key="include_cliente"):
     input_name = st.text_input(label="Insira o seu nome")
     input_age = st.number_input(label="Digite o valor do seu Deposito")
-    input_frutas = st.selectbox("Selecione a sua Fruta",["Morango","Maçã","Banana","Abacaxi","Acerola","Caja","Caju"])
+    input_frutas = st.selectbox("Selecione a sua Fruta",["Morango","Maçã","Banana","Abacaxi","Acerola","Caja","Caju","laranja"])
     input_button_submit = st.form_submit_button("Enviar") 
     
