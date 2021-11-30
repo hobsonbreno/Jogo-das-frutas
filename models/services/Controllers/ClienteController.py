0cb52397c45a0a75c98b712f9fa0a746c568af69
Hobson Nascimento <hobsonbmn@gmail.com>
2021-11-30 00:36:17 -0300

    atualização de campos formulario

--- models/services/Controllers/ClienteController.py
+++ models/services/Controllers/ClienteController.py
@@ -3,8 +3,8 @@ import services.database as db;
 
 def incluir(cliente):
     count = cursor.execute("""
-    INSERT INTO Cliente(cliNome,cliFrutas,cliDeposito)
+    INSERT INTO Cliente(cliNome,cliDeposito,cliAposta)
     VALUES(?,?,?)""",
-    cliente.nome, cliente.frutas, cliente.deposito).rowcount
+    cliente.nome, cliente.deposito, cliente.aposta).rowcount
     cnxn.commit()
 }
