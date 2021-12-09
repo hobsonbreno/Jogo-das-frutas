f41ee18224b64708b512845eae804d6e9c9069ca
Hobson Nascimento <hobsonbmn@gmail.com>
2021-12-08 22:14:10 -0300

    alteração do código sorteio

--- models/codigo_Sorteio.py
+++ models/codigo_Sorteio.py
@@ -7,14 +7,18 @@ lista_sorteio = []
 def deposito_isnumber(deposito):
     try:
         float(deposito)
+        if 0.1 <= deposito < 1.0:
+            return False
     except ValueError:
         return False
     return True
 
 
 def aposta_isnumber(aposta):
-    try:
+    try:          
         float(aposta)
+        if 0.1 <= aposta < 1.0:
+            return False
     except ValueError:
         return False
     return True        
@@ -43,7 +47,11 @@ while deposito_isnumber(deposito) is False:
     print('\neii sibite baleado aqui só aceita números seu lezado !!!')
     deposito = input('digite o deposite o valor do deposito:')
     deposito = deposito.replace(',','.')
+    
 aposta = input('digite o valor da aposta: ')
+aposta = aposta.replace(',','.')
+aposta = float(aposta)
+
 
 while aposta_isnumber(aposta) is False:
     print('\neii fi da chupi-chupi é pra digitar números !!!')
@@ -55,15 +63,12 @@ while verificar_aposta(aposta,deposito) is False:
     print('O seu saldo: R${:.2f}'.format(saldo))
     print('O seu aposta: R${:.2f}'.format(aposta))
     deposito = input('digite o valor do deposito: ')
+    saldo = deposito + saldo
 
 
 
 
-
-aposta = float(aposta)
-
-
-for i in range(4):
+for i in range(4):   
         sorteio = random.choice(frutas)
         lista_sorteio.append(sorteio)
 
@@ -77,7 +82,7 @@ elif lista_sorteio[0] == lista_sorteio[1] or lista_sorteio[2] == lista_sorteio[3
     saldo += aposta * 5 
     print('aposta * 5',saldo)
 elif lista_sorteio[0] != lista_sorteio[1] and lista_sorteio[2] != lista_sorteio[3]:
-    saldo = aposta + saldo
+    saldo = aposta - saldo
     print('\nvixi mah nem deu, só sobrou o buraco e a catinga',saldo)
 else:
     saldo -= aposta
